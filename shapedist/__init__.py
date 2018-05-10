58c080a32d8582ed1b78b954f500af0c732ef305
Kevin <kevin20841@gmail.com>
2018-05-10 16:33:09 -0400

    Code Revamp
    Updated script to include hierarchy input. Also updated uniform script. Corrected error in error function. Implemented data preprocessing.

--- shapedist/__init__.py
+++ shapedist/__init__.py
@@ -3,4 +3,5 @@ Shape Distance Package
 """
 from shapedist import elastic_n_2
 from shapedist import elastic_global
-from shapedist import elastic_linear
+#from shapedist import elastic_linear
+from shapedist import elastic_linear_old
